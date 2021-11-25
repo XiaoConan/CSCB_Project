d2fa6dd9c108504ea5e578e41be08ef22d128f23
Haoming-jpg <1669935064@qq.com>
2021-11-25 23:24:51 +0800

    1

--- app/src/main/java/com/example/cscb_project/MainActivity.java
+++ app/src/main/java/com/example/cscb_project/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         //meow
         //wong
         //edit on git
-        //test111
+        //test with sean
         setContentView(R.layout.activity_main);
     }
 }
