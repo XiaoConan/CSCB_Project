3b924763a6b6c0688af4f0c2cdda7d7ffd15d140
Irene-jpg <ireneirene.cao@mail.utoronto.ca>
2021-11-25 14:38:31 -0500

    basic Product class

---
+++ app/src/main/java/com/example/cscb_project/Product.java
@@ -0,0 +1,67 @@
+package com.example.cscb_project;
+
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private double price;
+    private String brand;
+
+    public Product() {
+        name = "";
+        price = 0;
+        brand = "";
+    }
+
+    public Product(String name, double price, String brand) {
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 && Objects.equals(name, product.name) && Objects.equals(brand, product.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, brand);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=$" + price +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+}
