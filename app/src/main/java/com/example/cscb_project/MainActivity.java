c1c13022ecacd7e877801e90c634255970f85c60
Haoming-jpg <1669935064@qq.com>
2021-11-25 16:51:40 +0800

    branch1

--- app/src/main/java/com/example/cscb_project/MainActivity.java
+++ app/src/main/java/com/example/cscb_project/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         //hello;
         //meow
         //wong
-        //test commit to another branch
+        //branch111111
         setContentView(R.layout.activity_main);
     }
 }
