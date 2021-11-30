d8b16d3cc92b7a4466761f902946bf0536af16e5
Haoming-jpg <1669935064@qq.com>
2021-12-01 02:36:12 +0800

    1

---
+++ app/src/main/java/com/example/cscb_project/StoreOwnerPage.java
@@ -0,0 +1,14 @@
+package com.example.cscb_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreOwnerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_owner_page);
+    }
+}
