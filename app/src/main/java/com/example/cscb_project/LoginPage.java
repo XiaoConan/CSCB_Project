7b613a8fac841f87e7f037e232f6a658f3e721cb
XiaoConan <HsupermarioM@gmail.com>
2021-12-02 14:07:47 -0500

    Changes to Main Page and Login Page

--- app/src/main/java/com/example/cscb_project/LoginPage.java
+++ app/src/main/java/com/example/cscb_project/LoginPage.java
@@ -2,7 +2,11 @@ package com.example.cscb_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +14,48 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+    }
+    public void login() {
+        Intent intent = getIntent();
+        String status = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        EditText un = (EditText) findViewById(R.id.editTextTextPersonName3);
+        String username = un.getText().toString();
+        EditText pw = (EditText) findViewById(R.id.editTextTextPassword);
+        String password = pw.getText().toString();
+        TextView check = findViewById(R.id.textView2);
+        if (username == "" || password == "") {
+            check.setText("Username or Password cannot be empty");
+        }
+        else if (status == "Store Owner") {
+            OwnerAccount owner = new OwnerAccount(username, password);
+            if (MainActivity.owners.contains(owner) == false) { //CHECK if firebase contains the username
+                check.setText("User not found");
+            }
+            else if () { //Check if the password matches the username
+
+            }
+            else { //If all conditions satisfied
+                intent = new Intent(this, CustomerPage.class);
+                startActivity(intent);
+            }
+        }
+        else {
+            if (MainActivity.customers.contains(username)) { //CHECK if firebase contains the username
+
+            }
+            else if () { //Check if the password matches the username
+
+            }
+            else { //If all conditions satisfied
+                intent = new Intent(this, CustomerPage.class);
+                startActivity(intent);
+            }
+
+        }
+
+    }
+    public void checkUsername() {
+
     }
 }
--- app/src/main/java/com/example/cscb_project/MainActivity.java
+++ app/src/main/java/com/example/cscb_project/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.cscb_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static ArrayList<OwnerAccount> owners;
+    public static ArrayList<CustomerAccount> customers;
+    public static final String EXTRA_MESSAGE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin() {
+        Intent intent = new Intent(this, LoginPage.class);
+        Spinner main_spinner = findViewById(R.id.spinner);
+        String status = String.valueOf(main_spinner.getSelectedItem());
+        intent.putExtra(EXTRA_MESSAGE, status);
+        startActivity(intent);
     }
 }
