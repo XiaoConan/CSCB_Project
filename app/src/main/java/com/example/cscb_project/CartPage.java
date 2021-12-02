e774ea204063d119587f01fd0a2a67b144d5cd30
SeanXiaoo <sean571354577@gmail.com>
2021-12-03 01:17:31 +0800

    Modify the CartPage Add sentOrder method Create DisplaySuccessfulMessage Activity

--- app/src/main/java/com/example/cscb_project/CartPage.java
+++ app/src/main/java/com/example/cscb_project/CartPage.java
@@ -2,13 +2,23 @@ package com.example.cscb_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CartPage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_page);
+    }
+
+    public void sentOrder(View view){
+        Intent intent = new Intent(this, DisplaySuccessfulMessage.class);
+
+        startActivity(intent);
     }
 }
