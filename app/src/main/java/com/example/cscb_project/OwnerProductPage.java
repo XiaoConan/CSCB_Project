433af61f846f1176908eb6fe20c620134fcfd921
sunnyjin2002 <sunnyjin2002@hotmail.com>
2021-12-04 00:10:30 +0800

    altered codes in OwnerProductPage.java

--- app/src/main/java/com/example/cscb_project/OwnerProductPage.java
+++ app/src/main/java/com/example/cscb_project/OwnerProductPage.java
@@ -20,25 +20,22 @@ public class OwnerProductPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_product_page);
-    }
-
-    public void displayItems(View view){
-        /* Read firebase for the items in the store, and display the name of them in the spinner.
-             the info of the item can be viewed by the store owner, or altered.
-           The last item of the spinner should be "Add Product"
-             and in this case, let the user enter the product name, price, brand.
+        /* Reads the firebase for the items in the store, and display the name of the products
+             in the spinner(and the first choice is "Add Product").
+           Should the user choose an item of the spinner, display the information accordingly
+             by getting the info of the product from firebase.
+           Leave empty if the spinner chooses "Add Product"
          */
-
-        // I need to get the owner info from previous page. Assume an Intent is passed with OwnerAccount.
         Intent intent = getIntent();
-        OwnerAccount owner = (OwnerAccount)intent.getSerializableExtra(LoginPage.MY_ACCOUNT);//get owner form Intent.;
-
+        OwnerAccount owner = (OwnerAccount)intent.getSerializableExtra(LoginPage.MY_ACCOUNT);
         Spinner spinner = findViewById(R.id.spinner);
+        String spinnerVal = String.valueOf(spinner.getSelectedItem());
+
         // read from firebase for the list of products.
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         // ** the following code is for a different program, requires a lot of change.
         //    and how to change it depends on how accounts are stored.
-        /*ref.child("areas").addValueEventListener(new ValueEventListener() {
+        ref.child("areas").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // Is better to use a List, because you don't know the size
@@ -53,8 +50,8 @@ public class OwnerProductPage extends AppCompatActivity {
 
                 //Spinner areaSpinner = (Spinner) findViewById(R.id.spinner);
                 ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(UAdminActivity.this, android.R.layout.simple_spinner_item, areas);
+                areasAdapter.add("Add Product"); //first choice.
                 areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                areasAdapter.add("Add Product");
                 spinner.setAdapter(areasAdapter);
             }
 
@@ -62,23 +59,60 @@ public class OwnerProductPage extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });*/
+        });
         // code ends. spinner should be populated by now.
 
+        // I need a loop to keep checking new products if the user switches the spinner.
+        while(false){
+            // if the spinner is not on "Add Product", display the info of the product
+            if (! spinnerVal.equals("Add Product")){
+
+            }
+            else{ // set the fields to empty
+
+            }
+        }
+    }
+
+    public void updateProduct(View view){
+        /* Triggered by the button "Confirm".
+           if the spinner is in "Add Product", add the specifications given as a product to the
+             store on firebase.
+           if the spinner is in another product, then pressing "Confirm" will update the
+             information of the product.
+         */
+
+        // I need to get the owner info from previous page. Assume an Intent is passed with OwnerAccount.
+        Intent intent = getIntent();
+        OwnerAccount owner = (OwnerAccount)intent.getSerializableExtra(LoginPage.MY_ACCOUNT);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        // read from firebase for the list of products.
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
         // now we fill in the fields.
-        String product = String.valueOf(spinner.getSelectedItem());
-        if (product.equals("Add Product")){ // new product
-            EditText productName = (EditText) findViewById(R.id.editTextTextProductName);
-            String name = productName.getText().toString();
+        String productName = String.valueOf(spinner.getSelectedItem());
+        if (productName.equals("Add Product")){ // new product
+            EditText productN = (EditText) findViewById(R.id.editTextTextProductName);
+            String name = productN.getText().toString();
             EditText productPrice = (EditText) findViewById(R.id.editTextTextProductPrice);
             double price = Double.parseDouble(productPrice.getText().toString());
             EditText productBrand = (EditText) findViewById(R.id.editTextTextProductBrand);
             String brand = productBrand.getText().toString();
             Product newProduct = new Product(name, price, brand, owner.getStore());
+            // add to firebase.
+            ref.child("products").child(productName).setValue(newProduct);
         }
         // Display the info of the product from firebase
         else{
-
+            Product product = ref.child("products").child(productName).getValue();
+            // ok, assuming I have retrieved productN
+            EditText productN = (EditText) findViewById(R.id.editTextTextProductName);
+            productN.setText(product.getName());
+            EditText productPrice = (EditText) findViewById(R.id.editTextTextProductName);
+            productN.setText(String.valueOf(product.getPrice()));
+            EditText productBrand = (EditText) findViewById(R.id.editTextTextProductName);
+            productN.setText(product.getBrand());
         }
     }
 }
