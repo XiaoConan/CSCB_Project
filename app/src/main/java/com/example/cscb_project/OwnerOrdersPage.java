ee37e3dfb5e1c1aca34992860988e1b1f96af821
Irene-jpg <ireneirene.cao@mail.utoronto.ca>
2021-12-03 01:20:51 -0500

    added some owner page connections, can't figure out how to display order @_@

--- app/src/main/java/com/example/cscb_project/OwnerOrdersPage.java
+++ app/src/main/java/com/example/cscb_project/OwnerOrdersPage.java
@@ -10,5 +10,7 @@ public class OwnerOrdersPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_orders_page);
+
+
     }
 }
--- app/src/main/java/com/example/cscb_project/StoreOwnerPage.java
+++ app/src/main/java/com/example/cscb_project/StoreOwnerPage.java
@@ -2,7 +2,9 @@ package com.example.cscb_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StoreOwnerPage extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class StoreOwnerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_owner_page);
+        // receive info from intent??
     }
+
+    /** Called when the user taps the View Products button */
+    public void goToProducts(View view) {
+        Intent intent = new Intent(this, OwnerProductPage.class);
+        // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the View Orders button */
+    public void goToOrders(View view) {
+        Intent intent = new Intent(this, OwnerOrdersPage.class);
+        // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
 }
