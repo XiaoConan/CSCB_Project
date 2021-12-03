9317e3dcfdd7c9ec828556d3f25a572116d84434
Haoming-jpg <1669935064@qq.com>
2021-12-03 16:49:05 +0800

    Add some customerpage connection

--- app/src/main/java/com/example/cscb_project/CustomerPage.java
+++ app/src/main/java/com/example/cscb_project/CustomerPage.java
@@ -2,7 +2,9 @@ package com.example.cscb_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CustomerPage extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class CustomerPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_page);
+    }
+
+    /** Called when the user taps the Go shop button */
+    public void goToShop(View view) {
+        Intent intent = new Intent(this, ShoppingStore.class);
+        // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps the My Cart button */
+    public void goToCart(View view) {
+        Intent intent = new Intent(this, CartPage.class);
+        // intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
