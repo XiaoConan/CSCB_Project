bed2632f3d18c2114b542b8ed44be6817d98cfb0
Irene-jpg <ireneirene.cao@mail.utoronto.ca>
2021-12-03 22:29:34 -0500

    Cleaned up example read/write Firebase code for everyone to model

--- app/src/main/java/com/example/cscb_project/MainActivity.java
+++ app/src/main/java/com/example/cscb_project/MainActivity.java
@@ -21,38 +21,49 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     public static final String USER_TYPE_MESSAGE = "USER_TYPE_MESSAGE";
 
+    public void exampleFirebaseCode() {
+        // EXAMPLE WRITE FROM FIREBASE
+        // We need a reference object to work with
+        // One line example: DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        // Two line example:
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference();
+        // Use the reference from parent directory + .child to get to the key you need
+        ref.child("Users").child("username").setValue("password");
+        // Or you can create a reference using a path String
+        ref = database.getReference("Stores");
+        ref.child("store").setValue("s");
+        String path = "/asdf";
+        ref = database.getReference("Products" + path + "/name");
+        ref.child("product").setValue("p");
+        ref = database.getReference("Orders");
+        ref.child("order").setValue("o");
+
+
+        // EXAMPLE READ FROM FIREBASE
+        // Basic get value
+        ref = database.getReference("Users/username");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                // If no data at location, getValue() returns null??
+                String value = snapshot.getValue(String.class);
+                // do something with value
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // This happens if getting the value failed... leave empty?
+                //Log.w("warning", "loadPost:onCancelled", error.toException());
+            }
+        });
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Write Random info to Firebase
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("Users");
-//        myRef.child("username").setValue("password");
-//        myRef = data.getReference("Stores");
-//        myRef.child("store").setValue("s");
-//        myRef = data.getReference("Products");
-//        myRef.child("product").setValue("p");
-//        myRef = data.getReference("Orders");
-//        myRef.child("order").setValue("o");
-//        //asdasdad
-
-
-
-        //Read from Firebase
-//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users/username");
-//        ref.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot snapshot) {
-//                String value = snapshot.getValue(String.class); // do something with value
-//            }
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                Log.w("warning", "loadPost:onCancelled", error.toException());
-//            }
-//        });
-
-
     }
 
     public void goToLoginPage(View view) {
