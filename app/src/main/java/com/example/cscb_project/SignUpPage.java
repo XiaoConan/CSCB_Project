d876c4442bb22b3221f58d84d9141ce4b632f882
Irene-jpg <ireneirene.cao@mail.utoronto.ca>
2021-12-04 03:47:46 -0500

    Fixed bug with too long messages crashing app. Fixed bug with not displaying successful message properly. Added regex restriction for username and error messages

--- app/src/main/java/com/example/cscb_project/SignUpPage.java
+++ app/src/main/java/com/example/cscb_project/SignUpPage.java
@@ -19,12 +19,12 @@ import java.util.regex.Pattern;
 public class SignUpPage extends AppCompatActivity {
     public static final String empty_username_error = "Please enter a username";
     public static final String empty_password_error = "Please enter a password";
-    public static final String invalid_username_error = "Username cannot contain spaces or special characters";
     public static final String username_taken_error = "Username already in use";
     public static final String success_message = "Successfully registered!";
-    public static final String validUsernameRegex = "\\w+";
-
-
+    public static final String invalid_username_error1 = "Username cannot contain special characters";
+    public static final String invalid_username_error2 = "Username must contain at least one letter/number";
+    public static final String validUsernameRegex1 = "\\w+"; // checking for anything with special character
+    public static final String validUsernameRegex2 = "\\w*[a-zA-Z0-9]\\w*"; // allows numbers, letters, underscores, but must have at least one letter/number
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,37 +55,37 @@ public class SignUpPage extends AppCompatActivity {
             display(empty_username_error);
         } else if (password.isEmpty()) {
             display(empty_password_error);
-        } else if (!Pattern.matches(validUsernameRegex, username)) {
-            display(invalid_username_error);
+        } else if (!Pattern.matches(validUsernameRegex1, username)) {
+            display(invalid_username_error1);
+        } else if (!Pattern.matches(validUsernameRegex2, username)) {
+            display(invalid_username_error2);
+        } else {
+            // Check if username already exists
+            usersRef.child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        display(username_taken_error);
+                    } else {
+                        // Add info to firebase
+                        usersRef.child(username).child("password").setValue(password);
+                        usersRef.child(username).child("type").setValue(userType);
+
+                        display(success_message);
+
+                        // Clear fields
+                        usernameEditText.setText("");
+                        passwordEditText.setText("");
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+
+                }
+            });
         }
 
-        // Check if username already exists
-        ValueEventListener listener = usersRef.child(username).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    display(username_taken_error);
-                } else {
-                    // Add info to firebase
-                    usersRef.child(username).child("password").setValue(password);
-                    usersRef.child(username).child("type").setValue(userType);
-
-                    display(success_message);
-
-                    // Clear fields
-                    usernameEditText.setText("");
-                    passwordEditText.setText("");
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-
-            }
-        });
-
-        // Close listener
-        usersRef.removeEventListener(listener);
     }
 
 }
